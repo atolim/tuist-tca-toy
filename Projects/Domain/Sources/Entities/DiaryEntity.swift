7155b61f92b8694622d6e5a4ad260b0eef7e41fb
ato <175409731+atolim@users.noreply.github.com>
2026-03-10 17:41:23 +0900

    refactor: unify Diary ID type to String

--- Projects/Domain/Sources/Entities/DiaryEntity.swift
+++ Projects/Domain/Sources/Entities/DiaryEntity.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 public struct DiaryEntity: Equatable, Identifiable {
-  public let id: UUID
+  public let id: String
   public var date: Date
   public var title: String
   public var content: String
   
-  public init(id: UUID = UUID(), date: Date, title: String, content: String) {
+  public init(id: String = UUID().uuidString, date: Date, title: String, content: String) {
     self.id = id
     self.date = date
     self.title = title
